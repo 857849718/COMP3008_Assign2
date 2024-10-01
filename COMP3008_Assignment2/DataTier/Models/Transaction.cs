ee44f004ca7d22592390f251b949b87051749bb8
George-PR <georgerowe05@gmail.com>
2024-10-01 17:53:06 +0800

    Created Transaction class in Models

---
+++ COMP3008_Assignment2/DataTier/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿namespace DataTier.Models
+{
+    public class Transaction
+    {
+        // class fields
+        private double depositAmount;
+        private double withdrawnAmount;
+        private List<string> transactionHistory;
+
+        // constructor
+        public Transaction(double depositAmount, double withdrawnAmount, List<string> transactionHistory)
+        {
+            this.depositAmount = depositAmount;
+            this.withdrawnAmount = withdrawnAmount;
+            this.transactionHistory = transactionHistory;
+        }
+
+        // accessors/mutators
+        public double DepositAmount { get; set; }
+        public double WithdrawnAmount { get; set; }
+        public List<string> TransactionHistory { get; set; }
+     }
+}
