0f60de212c9f85f85ae6d00b38cd7a380917c15a
George-PR <georgerowe05@gmail.com>
2024-10-02 15:54:38 +0800

    Added statements to create tables Accounts, Transactions, and Profiles in DatabaseManager

--- COMP3008_Assignment2/DataTier/Database/DatabaseManager.cs
+++ COMP3008_Assignment2/DataTier/Database/DatabaseManager.cs
@@ -15,7 +15,40 @@ namespace DataTier.Database
                     connection.Open();
                     using (SQLiteCommand command = connection.CreateCommand())
                     {
-                        
+                        command.CommandText = @"CREATE TABLE Accounts (
+                            AccountID INTEGER AUTOINCREMENT,
+                            Balance REAL,
+                            FirstName TEXT,
+                            LastName TEXT,
+                            PRIMARY KEY (AccountID)
+                        )";
+
+                        command.ExecuteNonQuery();
+
+                        command.CommandText = @"CREATE TABLE Transactions (
+                            TransactionID INTEGER AUTOINCREMENT,
+                            DepositAmount REAL,
+                            WithdrawnAmount REAL,
+                            AccountID INTEGER,
+                            PRIMARY KEY (TransactionID),
+                            FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+                        )";
+
+                        command.ExecuteNonQuery();
+
+                        command.CommandText = @"CREATE TABLE Profiles (
+                            FirstName TEXT,
+                            LastName TEXT,
+                            Email TEXT,
+                            Address TEXT,
+                            Phone INTEGER,
+                            Password TEXT,
+                            AccountID INTEGER,
+                            PRIMARY KEY (Email),
+                            FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+                        )";
+
+                        command.ExecuteNonQuery();
                     }
                 }
             }
