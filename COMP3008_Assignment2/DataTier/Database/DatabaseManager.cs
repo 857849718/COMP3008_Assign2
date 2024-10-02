374db924077d5f239527dae10ca439e37e53a791
George-PR <georgerowe05@gmail.com>
2024-10-02 15:12:34 +0800

    Created database bank.db in DataTier

--- COMP3008_Assignment2/DataTier/Database/DatabaseManager.cs
+++ COMP3008_Assignment2/DataTier/Database/DatabaseManager.cs
@@ -1,7 +1,10 @@
-﻿namespace DataTier.Database
+﻿using System.Data.SQLite;
+
+namespace DataTier.Database
 {
     public class DatabaseManager
     {
-        string connectString = "Data Source = mydatabase.db;Version = 3";
+        string connectString = "Data Source=bank.db;Version=3;";
+        SQLiteConnection connection = new SQLiteConnection(connectString);
     }
 }
