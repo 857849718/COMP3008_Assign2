656b034a2830232b40c329d1ae2541ce131086c6
George-PR <georgerowe05@gmail.com>
2024-10-06 19:17:52 +0800

    Finished TransactionsOps.GetTransactionsByID

--- COMP3008_Assignment2/DataTier/Database/TransactionsOps.cs
+++ COMP3008_Assignment2/DataTier/Database/TransactionsOps.cs
@@ -90,5 +90,46 @@ namespace DataTier.Database
             // return list containing all transactions
             return transactionList;
         }
+
+        public static List<Transaction> GetTransactionsByID(int accountID)
+        {
+            // initalize list of transactions
+            List<Transaction> transactionList = new List<Transaction>();
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        // query to retrieve transactions based on primary key
+                        command.CommandText = "SELECT * FROM Transactions WHERE AccountID = @AccountID";
+                        command.Parameters.AddWithValue("@AccountID", accountID);
+
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // create new transaction with read values
+                                Transaction transaction = new Transaction(
+                                    Convert.ToDouble(reader["Amount"]),
+                                    Convert.ToInt32(reader["AccountID"])
+                                );
+
+                                // add transaction to transactionList
+                                transactionList.Add(transaction);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception oof)
+            {
+                Console.WriteLine("Error: " + oof.Message);
+            }
+
+            return transactionList;
+        }
     }
 }
