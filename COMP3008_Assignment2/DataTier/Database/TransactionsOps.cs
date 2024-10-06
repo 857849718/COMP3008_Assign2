a4c58e83bf53c928a96479c691eb1fe041ce61ff
George-PR <georgerowe05@gmail.com>
2024-10-06 19:08:03 +0800

    Started TransactionOps.Insert and modified Transaction class - now holds attribute accountID

--- COMP3008_Assignment2/DataTier/Database/TransactionsOps.cs
+++ COMP3008_Assignment2/DataTier/Database/TransactionsOps.cs
@@ -1,4 +1,7 @@
-﻿namespace DataTier.Database
+﻿using DataTier.Models;
+using System.Data.SQLite;
+
+namespace DataTier.Database
 {
     /*  class used to perform operations
      *  on the Transactions table;
@@ -7,5 +10,43 @@
     {
         private static string connectString = "Data Source=bank.db;Version=3;";
 
+        public static bool Insert(Transaction transaction)
+        {
+            try
+            {
+                // create new SQLite connection
+                using (SQLiteConnection connection = new SQLiteConnection(connectString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = connection.CreateCommand())
+                    {
+                        // command parameters
+                        command.CommandText = @"
+                        INSERT INTO Transactions (Amount, AccountID)
+                        VALUES (@Amount, @AccountID)";
+
+                        // pararmeters inserted
+                        command.Parameters.AddWithValue("@Transactions", transaction.Amount);
+                        command.Parameters.AddWithValue("@FirstName", account.FirstName);
+
+                        int rowsInserted = command.ExecuteNonQuery();
+
+                        // ensure record has been inserted
+                        if (rowsInserted > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch (Exception oof)
+            {
+                Console.WriteLine("Error: " + oof.Message);
+                return false;
+            }
+
+            return false;
+        }
     }
 }
--- COMP3008_Assignment2/DataTier/Models/Transaction.cs
+++ COMP3008_Assignment2/DataTier/Models/Transaction.cs
@@ -4,14 +4,17 @@
     {
         // class fields
         private double amount; // deposited/withdrawn amount
+        private int accountID;
 
         // constructor
-        public Transaction(double amount)
+        public Transaction(double amount, int accountID)
         {
             this.amount = amount;
+            this.accountID = accountID;
         }
 
         // accessors/mutators
         public double Amount { get; set; }
+        public int AccountID { get; set; }
      }
 }
