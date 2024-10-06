a8c82579612decaddbf7c6b6aae206d56670f7ab
George-PR <georgerowe05@gmail.com>
2024-10-06 18:58:28 +0800

    Created class TransactionOps to perform operations on the Transactions table

---
+++ COMP3008_Assignment2/DataTier/Database/TransactionsOps.cs
@@ -0,0 +1,11 @@
+﻿namespace DataTier.Database
+{
+    /*  class used to perform operations
+     *  on the Transactions table;
+     */
+    public class TransactionsOps
+    {
+        private static string connectString = "Data Source=bank.db;Version=3;";
+
+    }
+}
