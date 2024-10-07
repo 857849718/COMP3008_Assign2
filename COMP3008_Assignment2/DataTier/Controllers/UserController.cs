a2bca25d92b1b545a48041e0d0c6c14d6e4b5b15
857849718 <hhw857849718@gmail.com>
2024-10-07 19:41:38 +0800

    Acc, Transac, User controller implemented (on DataTier for now)

---
+++ COMP3008_Assignment2/DataTier/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using DataTier.Database;
+using DataTier.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataTier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // create new profile
+        [HttpPost]
+        public IActionResult CreateNewProfile([FromBody] UserProfile profile)
+        {
+            if (ProfilesOps.Insert(profile))
+            {
+                return Ok("User profile successfully created");
+            }
+            return BadRequest("User profile creation failed");
+        }
+
+        // retrieve user profile by email
+        [HttpGet]
+        public IActionResult GetProfileByEmail(string email)
+        {
+            UserProfile profile = ProfilesOps.GetProfileByEmail(email);
+            if (profile != null)
+            {
+                return Ok(profile);
+            }
+            return NotFound();
+        }
+
+        // update profile
+        [HttpPatch]
+        public IActionResult UpdateProfile([FromBody] UserProfile profile)
+        {
+            if(ProfilesOps.Update(profile))
+            {
+                return Ok("User profile successfully updated");
+            }
+            return BadRequest("User profile update failed");
+        }
+
+        // delete profile
+        [HttpDelete]
+        public IActionResult DeleteProfile(string email)
+        {
+            if(ProfilesOps.Delete(email))
+            {
+                return Ok("User profile successfully deleted");
+            }
+            return BadRequest("User profile delete failed");
+        }
+    }
+}
