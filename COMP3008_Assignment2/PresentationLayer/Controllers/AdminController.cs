76ee3d8e893ce1cadcd5a754d9295d5f6c804188
George-PR <georgerowe05@gmail.com>
2024-10-13 21:08:33 +0800

    ShowAdminDashboard in AdminController

--- COMP3008_Assignment2/PresentationLayer/Controllers/AdminController.cs
+++ COMP3008_Assignment2/PresentationLayer/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Intermed;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace PresentationLayer.Controllers
 {
@@ -11,7 +13,25 @@ namespace PresentationLayer.Controllers
         [HttpGet("ShowAdminDashboard")]
         public IActionResult ShowAdminDashboard()
         {
+            UserProfileIntermed adminProfile = GetProfileByEmail("admin@bankingsolutions.com");
+            ViewBag.adminName = adminProfile.FirstName;
+            ViewBag.adminEmail = adminProfile.Email;
+            ViewBag.adminPhone = adminProfile.Phone; 
             return PartialView("AdminDashboard");
+        }
+
+        public UserProfileIntermed GetProfileByEmail(string email)
+        {
+            RestClient restClient = new RestClient("http://localhost:5186");
+            var request = new RestRequest($"/api/user/get/{email}", Method.Get);
+            RestResponse response = restClient.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                var profile = JsonConvert.DeserializeObject<UserProfileIntermed>(response.Content);
+                return profile;
+            }
+            return null;
         }
     }
 }
