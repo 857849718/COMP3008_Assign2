4c8150ba1ab2bc067486abb344b58e22ae66069f
George-PR <georgerowe05@gmail.com>
2024-10-13 21:25:27 +0800

    Added event listeners for buttons in admin dashboard

--- COMP3008_Assignment2/PresentationLayer/wwwroot/js/adminDashboard.js
+++ COMP3008_Assignment2/PresentationLayer/wwwroot/js/adminDashboard.js
@@ -1,1 +1,6 @@
-﻿
+﻿document.getElementById("viewUsers").addEventListener("click", function () {
+    loadView("AllUsers");
+});
+document.getElementById("viewTransactions").addEventListener("click", function () {
+    loadView("AllTransactions");
+})
