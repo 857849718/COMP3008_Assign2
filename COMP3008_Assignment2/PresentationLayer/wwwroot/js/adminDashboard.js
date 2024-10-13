44e43622ac610afbc3def562990a40ae005f5d78
George-PR <georgerowe05@gmail.com>
2024-10-13 22:56:07 +0800

    Changed view all users to button to use onclick() directly in html

--- COMP3008_Assignment2/PresentationLayer/wwwroot/js/adminDashboard.js
+++ COMP3008_Assignment2/PresentationLayer/wwwroot/js/adminDashboard.js
@@ -1,8 +1,5 @@
 ﻿console.log("admin dashboard loaded");
 
-const viewUsersButton = document.getElementById("viewUsers");
-viewUsersButton.addEventListener("click", test);
-
 function test() {
     console.log("clicked");
     alert("HMMMM");
